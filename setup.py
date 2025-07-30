1696200b6145286bf3764653bc0a6dbfac38fbdf
Kris Surette <104463248+VTskier@users.noreply.github.com>
2025-07-30 11:34:19 -0400

    made edits according to feedback from J

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.VTSmallClaimsComplaint',
       license='The MIT License',
       url='https://VTLawHelp.org/VTCourtForms',
       packages=find_namespace_packages(),
-      install_requires=['docassemble.ALToolbox>=0.11.1', 'docassemble.AssemblyLine>=3.2.0', 'docassemble.GithubFeedbackForm>=0.4.1.1', 'docassemble.VTFeedback', 'docassemble.VTSharedYMLFile'],
+      install_requires=['docassemble.ALToolbox @ git+https://github.com/SuffolkLITLab/docassemble-ALToolbox.git@main', 'docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main', 'docassemble.VTFeedback @ git+https://github.com/VTskier/docassemble-VTFeedback.git@main', 'docassemble.VTSharedYMLFile @ git+https://github.com/LSVermont/docassemble-VTSharedYMLFile.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VTSmallClaimsComplaint/', package='docassemble.VTSmallClaimsComplaint'),
      )
