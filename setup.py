c3d1f6509203408336576c9f7e512f13f6475564
Kris Surette <104463248+VTskier@users.noreply.github.com>
2025-09-15 13:39:52 -0400

    work on incorporating other party's mailing address per R

--- setup.py
+++ setup.py
@@ -50,11 +50,10 @@ setup(name='docassemble.VTSmallClaimsComplaint',
       long_description_content_type='text/markdown',
       author='Guided interview by Legal Services Vermont / VTCourtForms',
       author_email='ksurette@legalservicesvt.org',
-      license='The MIT License',
+      license='MIT',
       url='https://VTLawHelp.org/VTCourtForms',
       packages=find_namespace_packages(),
       install_requires=['docassemble.ALToolbox @ git+https://github.com/SuffolkLITLab/docassemble-ALToolbox.git@main', 'docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main', 'docassemble.VTFeedback @ git+https://github.com/VTskier/docassemble-VTFeedback.git@main', 'docassemble.VTSharedYMLFile @ git+https://github.com/LSVermont/docassemble-VTSharedYMLFile.git@main'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/VTSmallClaimsComplaint/', package='docassemble.VTSmallClaimsComplaint'),
      )
-
